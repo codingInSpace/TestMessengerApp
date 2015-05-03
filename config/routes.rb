4dfd17728b6da1178d976a4ae3653a139d218598
jonathan <jonathan.grangien@gmail.com>
2015-05-03 23:01:04 +0200

    add route to messages new action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get '/messages' => 'messages#index'
+  get '/messages/new' => 'messages#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
