e1e923f04c6dbee7102715c38e269259248cca16
jonathan <jonathan.grangien@gmail.com>
2015-05-03 23:08:18 +0200

    add create action to message controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,6 +7,15 @@ class MessagesController < ApplicationController
 		@message = Message.new
 	end
 
+	def create 
+		@message = Message.new(message_params) 
+		if @message.save 
+			redirect_to '/messages' 
+		else 
+			render 'new' 
+		end 
+	end
+
 	private 
 	def message_params 
 		params.require(:message).permit(:content) 
